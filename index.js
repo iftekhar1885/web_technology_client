c0e783e6d00a89d49ab747a376c2c831407d1a95
mdefat233 <kharifte16@gmail.com>
2022-10-25 23:33:03 +0600

    json folder added

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const courses = require('./data/Course.json');
 
 app.get('/', (req, res) =>{
     res.send('Web API Running');
@@ -15,6 +16,29 @@ app.get('/course-categories', (req, res)=> {
     res.send(categories)
 
 })
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courses.find( c => c._id === id);
+    res.send(selectedCourse);
+
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(courses)
+    }
+    else{
+        const category_course = courses.filter( c => c.category_id === id);
+        res.send(category_course);
+    }
+    
+})
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+})
+
 app.listen(port, () =>{
     console.log('web technology server running on port', port);
 })
